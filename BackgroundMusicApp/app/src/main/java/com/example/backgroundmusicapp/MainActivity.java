9ceb30bdefb78199a1e90e01b008d4007c011a32
jeongminP <pjm9355@naver.com>
2021-04-22 14:27:24 +0900

    서비스 테스트 실습

---
+++ BackgroundMusicApp/app/src/main/java/com/example/backgroundmusicapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.backgroundmusicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    Button btnStart, btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("백그라운드 서비스 음악 앱");
+
+        intent = new Intent(this, MusicService.class);
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+                android.util.Log.i("서비스 테스트", "startService()");
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+                android.util.Log.i("서비스 테스트", "stopService()");
+            }
+        });
+    }
+}
